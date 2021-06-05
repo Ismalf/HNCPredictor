d5cf5cff9cb68073085534c53b3cba7d6b6613c5
Ismael <ismalfmp@gmail.com>
2021-06-04 22:00:12 -0500

    Tested core functionality

--- core.js
+++ core.js
@@ -1,38 +1,130 @@
+
+const START_OF_RESTRICTION_FIRST_HALF = 700 // As 7:00 AM but easier to compare
+const END_OF_RESTRICTION_FIRST_HALF = 930 // As 9:30 AM but easier to compare
+
+const START_OF_RESTRICTION_SECOND_HALF = 1600 // As 4:00 PM but easier to compare
+const END_OF_RESTRICTION_SECOND_HALF = 1930 // As 7:30 PM but easier to compare
+
+const plateNumberInput = document.getElementById('plateNumber')
+const circulationDateTimeInput = document.getElementById('circulationDateTime')
+const results = document.getElementById('results')
+
 const check = () => {
-
-    const el = document.getElementById('results')
-    el.classList.add('grow')
-    el.innerHTML = 'Ok JS'
+    reset()
     const { plateNumber, date } = getInputValues()
-    const plateCharacteristics = extractCharacteristics(plateNumber)
-    console.log(plateCharacteristics, date)
+    const errs = inputHasErrors(plateNumber, date)
+    if (errs.errs) {
+        errs.inputWErr.forEach(input => setState(input, true, false))
+        return
+    }
+    const prediction = getPrediction(extractCharacteristics(plateNumber), date)
+    showPrediction(prediction)
+}
 
+const setState = (input = null, hasErrors = false, isReset = true) => {
+    if (isReset) {
+        input.classList.remove('inputOk')
+        input.classList.remove('inputErr')
+        input.value = ''
+    } else {
+        if (hasErrors) {
+            input.classList.remove('inputOk')
+            input.classList.add('inputErr')
+        } else {
+            input.classList.remove('inputErr')
+            input.classList.add('inputOk')
+        }
+    }
 }
 
 const getInputValues = () => {
-    const plateNumber = document.getElementById('plateNumber').value
-    const date = document.getElementById('circulationDateTime').value
+    const plateNumber = plateNumberInput.value
+    const date = circulationDateTimeInput.value
     return { plateNumber, date }
+}
+
+const inputHasErrors = (plate, date) => {
+    let errs = 0
+    let inputWErr = []
+    if (!plate || !/^[A-Z]{2,3}[0-9]{3,4}[A-Z]{0,1}$/.test(plate)) {
+        errs++
+        inputWErr.push(plateNumberInput)
+    }
+    if (!date) {
+        errs++
+        inputWErr.push(circulationDateTimeInput)
+    }
+    console.log(inputWErr)
+    return { errs, inputWErr }
+}
+
+const getPrediction = (characteristics, date) => {
+
+    const vhType = `Vehicle type: ${characteristics.type === 'c' ? 'Car' : 'Motor bike'}`
+    const province = `${characteristics.province === 'P' ? 'From' : 'Visiting'}: Pichincha`
+    const canBeOnRoad = isAbleToRide(characteristics.lastDigit, date) ? 'You are able to use your vehicle' : 'You are not able to use your vehicle'
+    return `${vhType} <br/> ${province} <br/> ${canBeOnRoad}`
+
+}
+
+const isAbleToRide = (lastDigit, date) => {
+    const dateTime = new Date(date)
+    console.log(date)
+    const dayOfWeek = dateTime.getDay()
+    const hour = dateTime.getHours() * 100 + dateTime.getMinutes()
+    if (!((START_OF_RESTRICTION_FIRST_HALF <= hour && END_OF_RESTRICTION_FIRST_HALF > hour)
+        || (START_OF_RESTRICTION_SECOND_HALF <= hour && END_OF_RESTRICTION_SECOND_HALF > hour))) {
+        return true
+    }
+    switch (dayOfWeek) {
+        case 1:
+            return lastDigit !== 1 && lastDigit !== 2
+        case 2:
+            return lastDigit !== 3 && lastDigit !== 4
+        case 3:
+            return lastDigit !== 5 && lastDigit !== 6
+        case 4:
+            return lastDigit !== 7 && lastDigit !== 8
+        case 5:
+            return lastDigit !== 9 && lastDigit !== 0
+        default:
+            return true
+    }
 }
 
 const extractCharacteristics = (plate) => {
 
     const characteristics = {
         type: null, // To Know if it's a car or a bike
-        lastDigit: null // The plate last digit
+        lastDigit: null, // The plate last digit
+        province: null
     }
 
-    const carPlatePattern = /[A-Z]{2,3}[0-9]{4}/ // cars
-    const bikePlatePattern = /[A-Z]{2}[0-9]{3}[A-Z]{1}/ // bikes
+    const carPlatePattern = /^[A-Z]{2,3}[0-9]{4}$/ // cars
+    const bikePlatePattern = /^[A-Z]{2}[0-9]{3}[A-Z]{1}$/ // bikes
 
     if (carPlatePattern.test(plate)) {
         characteristics.type = 'c'
-        characteristics.lastDigit = plate[plate.length - 1]
+        characteristics.lastDigit = +plate[plate.length - 1]
     } else if (bikePlatePattern.test(plate)) {
         characteristics.type = 'b'
-        characteristics.lastDigit = plate[plate.length - 2]
+        characteristics.lastDigit = +plate[plate.length - 2]
     }
+    characteristics.province = plate[0]
     return characteristics
 }
 
-module.exports = { extractCharacteristics }
+const showPrediction = (prediction) => {
+    results.classList.add('grow')
+    results.innerHTML = prediction
+}
+
+const reset = (newSearch = true) => {
+    const results = document.getElementById('results')
+    results.classList.remove('grow')
+    results.innerHTML = ''
+    setState(plateNumberInput, false, !newSearch)
+    setState(circulationDateTimeInput, false, !newSearch)
+}
+
+module.exports.default = { extractCharacteristics, inputHasErrors, isAbleToRide }
--- test/core.test.js
+++ test/core.test.js
@@ -1,25 +1,81 @@
-const app = require('../core.js')
-describe('Pico y Placa predictor', () => {
+/**
+ * @jest-environment jsdom
+ */
+const app = require('../core.js').default
+describe('licence plate number validation', () => {
 
     it('should identify a diplomatics car\'s plate number', () => {
         const result = app.extractCharacteristics('CD1443')
-        
-        const expected = { type: 'c', lastDigit: '3' }
+
+        const expected = { type: 'c', lastDigit: 3, province: 'C' }
         expect(result).toEqual(expected)
     })
 
     it('should identify a private car\'s plate number', () => {
         const result = app.extractCharacteristics('PDB1443')
-        
-        const expected = { type: 'c', lastDigit: '3' }
+
+        const expected = { type: 'c', lastDigit: 3, province: 'P' }
+        expect(result).toEqual(expected)
+    })
+
+    it('should identify a bike\'s plate number', () => {
+        const result = app.extractCharacteristics('PD144G')
+
+        const expected = { type: 'b', lastDigit: 4, province: 'P' }
         expect(result).toEqual(expected)
     })
     
     it('should identify a bike\'s plate number', () => {
-        const result = app.extractCharacteristics('PD144G')
-        
-        const expected = { type: 'b', lastDigit: '4' }
+        const result = app.extractCharacteristics('PD144GASD')
+
+        const expected = { type: null, lastDigit: null, province: 'P' }
         expect(result).toEqual(expected)
     })
 
+})
+
+describe('Restriction date and time validations', () => {
+    it('should identify a non restricted timeframe to use a vehicle', () => {
+        const isAbleToMove = app.isAbleToRide(9,'2021-06-04T20:48')
+        expect(isAbleToMove).toBe(true)
+    })
+    it('should identify a non restricted timeframe to use a vehicle (2)', () => {
+        const isAbleToMove = app.isAbleToRide(1,'2021-06-04T10:48')
+        expect(isAbleToMove).toBe(true)
+    })
+    it('should identify a restricted timeframe to use a vehicle (first half)', () => {
+        const isAbleToMove = app.isAbleToRide(9,'2021-06-04T08:18')
+        expect(isAbleToMove).toBe(false)
+    })
+    it('should identify a restricted timeframe to use a vehicle (second half)', () => {
+        const isAbleToMove = app.isAbleToRide(9,'2021-06-04T17:48')
+        expect(isAbleToMove).toBe(false)
+    })
+    it('should identify as non restricted timeframe for a licence plate number that has no restriction a particular day (first half)', () => {
+        const isAbleToMove = app.isAbleToRide(8,'2021-06-04T08:18')
+        expect(isAbleToMove).toBe(true)
+    })
+    it('should identify as non restricted timeframe for a licence plate number that has no restriction a particular day (second half)', () => {
+        const isAbleToMove = app.isAbleToRide(8,'2021-06-04T17:48')
+        expect(isAbleToMove).toBe(true)
+    })
+})
+
+describe('Input validations', () => {
+    it('Correct date, incorrect licence plate number ', () => {
+        const errs = app.inputHasErrors('ci8990b','2021-06-04T20:48').errs
+        expect(errs).toBe(1)
+    })
+    it('Incorrect date, correct licence plate number', () => {
+        const errs = app.inputHasErrors('CD8990',null).errs
+        expect(errs).toBe(1)
+    })
+    it('Correct licence and date', () => {
+        const errs = app.inputHasErrors('PWD0295','2021-06-04T20:48').errs
+        expect(errs).toBe(0)
+    })
+    it('Incorrect date, incorrect licence plate number', () => {
+        const errs = app.inputHasErrors('PW0295d',null).errs
+        expect(errs).toBe(2)
+    })
 })
